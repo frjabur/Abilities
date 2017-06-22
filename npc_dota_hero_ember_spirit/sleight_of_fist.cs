84ed5cea6302975a1c07616f499cc18a559f81c4
frjabur <frjabur@gmail.com>
2017-06-22 17:48:10 -0300

    t
    t

---
+++ npc_dota_hero_ember_spirit/sleight_of_fist.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="sleight_of_fist.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_ember_spirit
+{
+    public class ember_spirit_sleight_of_fist : CircleAbility
+    {
+        public ember_spirit_sleight_of_fist(Ability ability)
+            : base(ability)
+        {
+        }
+
+}
