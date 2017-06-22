5cc54a8619092327cb2434cc5f289a67e2e8261f
frjabur <frjabur@gmail.com>
2017-06-22 18:21:22 -0300

    Update sleight_of_fist.cs

--- npc_dota_hero_ember_spirit/sleight_of_fist.cs
+++ npc_dota_hero_ember_spirit/sleight_of_fist.cs
@@ -4,11 +4,14 @@
 
 namespace Ensage.SDK.Abilities.npc_dota_hero_ember_spirit
 {
-    public class ember_spirit_sleight_of_fist : CircleAbility
+    public class ember_spirit_sleight_of_fist : CircleAbility, IHasModifier
     {
         public ember_spirit_sleight_of_fist(Ability ability)
             : base(ability)
         {
         }
+        
+        public string ModifierName { get; } = "modifier_ember_spirit_sleight_of_fist_caster";
+    }
 
 }
